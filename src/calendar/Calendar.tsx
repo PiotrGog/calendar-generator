ed9dd020d433274121bf8050a9e6f48abef26683
PiotrGog <gogola.piotr@interia.pl>
2024-11-26 22:31:13 +0100

    Create Cards

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { toPng } from 'html-to-image';
+
 import EmptyCard from './EmptyCard';
 import styles from './Calendar.module.css'
 import Card from './Card';
@@ -9,34 +11,92 @@ type CalendarState = {
 }
 
 class Calendar extends React.Component<{}, CalendarState> {
-    static DAYS: string[] = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
+    static DAYS: string[] = [
+        'Niedziela',
+        'Poniedziałek',
+        'Wtorek',
+        'Środa',
+        'Czwartek',
+        'Piątek',
+        'Sobota'
+    ];
+    static MONTHS: string[] = [
+        'Styczeń',
+        'Luty',
+        'Marzec',
+        'Kwiecień',
+        'Maj',
+        'Czerwiec',
+        'Lipiec',
+        'Sierpień',
+        'Wrzesień',
+        'Październik',
+        'Listopad',
+        'Grudzień'
+    ];
+
+    private elementRef = React.createRef<HTMLDivElement>();
+    private month: number = 0;
 
     constructor(props: any) {
         super(props);
-        const month = 10;
+        this.state = {
+            days: this.prepareDaysForCurrentMonth(),
+        };
+    }
 
+    monthChange(e: any) {
+        this.month = +e.target.value;
+
+        this.setState({
+            days: this.prepareDaysForCurrentMonth()
+        });
+    }
+
+    prepareDaysForCurrentMonth() {
         let days = []
-        for (let d = new Date(2024, month, 1); d.getMonth() === month; d.setDate(d.getDate() + 1)) {
+        for (let d = new Date(2024, this.month, 1); d.getMonth() === this.month; d.setDate(d.getDate() + 1)) {
             days.push(new Date(d));
         }
+        return days;
+    }
 
-        this.state = {
-            days: days
-        };
+    save() {
+        toPng(this.elementRef.current as HTMLElement, { cacheBust: false })
+            .then((dataUrl) => {
+                const link = document.createElement("a");
+                link.download = "my-image-name.png";
+                link.href = dataUrl;
+                link.click();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     render() {
         return (
-            <div className={styles.calendarMain}>
-                <div className={styles.calendarContainer}>
-                    {Calendar.DAYS.map(d =>
-                        <div className={styles.dayCard} key={`${d}`}>{d}</div>
-                    )}
-                    {[...Array(this.state.days[0].getDay())].map((x, i) => <EmptyCard key={i} />
-                    )}
-                    {this.state.days.map(d =>
-                        <Card key={`${d}`} date={d} />
-                    )}
+            <div>
+                <div ref={this.elementRef} className={styles.calendarMain}>
+                    <div className={styles.calendarHeader}>{Calendar.MONTHS[this.month]}</div>
+                    <div className={styles.calendarContainer}>
+                        {Calendar.DAYS.map(d =>
+                            <div className={styles.dayCard} key={`${d}`}>{d}</div>
+                        )}
+                        {[...Array(this.state.days[0].getDay())].map((x, i) => <EmptyCard key={i} />
+                        )}
+                        {this.state.days.map(d => {
+                            return <Card key={`${d}`} date={d} />
+                        })}
+                    </div>
+                </div>
+                <div className={styles.userPanel}>
+                    <select className={styles.monthSelector} onChange={this.monthChange.bind(this)}>
+                        {Calendar.MONTHS.map((x, i) =>
+                            <option value={i} key={i}>{x}</option>
+                        )}
+                    </select>
+                    <button className={styles.generateButton} onClick={this.save.bind(this)}>Generuj</button>
                 </div>
             </div>
         )
